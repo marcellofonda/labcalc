3cf58c7f52fed1ff785c0f660408015b9bf415bd
Marcello Fonda <marcellofonda@gmail.com>
2021-04-09 09:05:27 +0000

    09_04_2021-2.f90

---
+++ 09_04_2021-2.f90
@@ -0,0 +1,15 @@
+program esempio2
+
+integer :: ia,ib,ic
+real    :: rx,ry,rz
+
+print*,' ia, rx ?'
+read*, ia, rx
+ry = ia + rx ! per sommare un intero e un reale converte prima l'intero in reale
+ib = ia + rx ! per salvare il risultato della somma, la parte 
+print*, ry, ib
+rz = ia/ib ! la divisione è tra interi, quindi il risultato è un intero, che poi viene convertito
+print*,'rz ',rz
+
+
+end program esempio2
