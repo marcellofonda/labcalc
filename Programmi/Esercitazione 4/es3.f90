7d501361c401fe86eea867fc7b26a6fde4f6c53e
Marcello <40807932+marcellofonda@users.noreply.github.com>
2021-04-27 15:03:11 +0200

    Aggiornato esercizio 3
    Aggiunto il termine gamma per confrontare la somma con il logaritmo in modo più affidabile.

--- Programmi/Esercitazione 4/es3.f90
+++ Programmi/Esercitazione 4/es3.f90
@@ -1,14 +1,16 @@
 program insomma
 integer :: N, i, j
-real :: somma1=0, somma2=0, ln
-
+real :: somma1=0, somma2=0, ln, gamma=0.577215664901532860606512090082402431042159335939923598805767234884867726777664670937
+                        
 do j=1,100
 	N=j*10000
 	do i=1,N
 		somma1 = somma1 + 1./i
 		somma2 = somma2 + 1./(N-i+1)
 	end do
-	ln=log(real(N))
+	ln=log(real(N)) + gamma 	!La sommatoria ha lo stesso andamento asintotico dell'integrale
+								!da 1 a N. La differenza tra le due tende a un valore costante gamma.
+								!L'integrale vale il logaritmo naturale di N
 	print*, "I due risultati sono, per N=", N
 	print*,somma1, somma2
 	print*, "e differiscono di", somma1-somma2
